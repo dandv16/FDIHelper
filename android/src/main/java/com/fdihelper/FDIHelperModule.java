cc35aaf82c8c2ca4ff886448baf03e4431b13be4
AdminPG <adminpgsoftware@Macs-MacBook-Pro.local>
2023-05-10 09:37:28 +0700

    chore: initial commit

---
+++ android/src/main/java/com/fdihelper/FDIHelperModule.java
@@ -0,0 +1,32 @@
+package com.fdihelper;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = FDIHelperModule.NAME)
+public class FDIHelperModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "FDIHelper";
+
+  public FDIHelperModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  public void multiply(double a, double b, Promise promise) {
+    promise.resolve(a * b);
+  }
+}
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  FDIHelperExample
+//
+
+import Foundation
